ca81ef1a964359a89a93f2af7a5695bd81d26560
Kendrick Taylor <sktayloriii@gmail.com>
2012-06-11 23:17:10 -0700

    add superagent to underscore

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 echo "(function(){" > dry.underscore.js
 cat ./deps/underscore/underscore.js >> dry.underscore.js
+cat ./deps/superagent/superagent.js >> dry.underscore.js
 #cat ./deps/underscore.string/dist/underscore.string.min.js >> dry.underscore.js
 echo "(" >> dry.underscore.js
 node -e "console.log(require('./lib/common.js').mixin.toString())" >> dry.underscore.js
