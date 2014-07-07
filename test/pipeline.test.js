02a0da2c022fb7e256fc7202e114657add5953fb
Kendrick Taylor <kendrick@circuitsix.com>
2014-07-06 22:36:49 -0700

    fix pipeline bug

--- test/pipeline.test.js
+++ test/pipeline.test.js
@@ -15,15 +15,20 @@ function testPipeline(beforeExit){
     p.before("2", "1");
     p.after("2", "3");
     p.before("5", "4");
-    p.before("4", "3");
     p.after("6", "7");
+    p.before("4", "3");
     p.before("6", "5");
     p.before("1", "0");
+    p.before("1", "0.5");
 
     try{ p.order(); }
-    catch(e){ ok(false); }
+    catch(e){ 
+        p.printNodes();
+        _.p(e.message);
+        ok(false);
+    }
 
-    eq(p.order(), ['0', '1', '2', '3', '4', '5', '6', '7']);
+    eq(p.order(), ['0', '0.5', '1', '2', '3', '4', '5', '6', '7']);
 
     p.before("9", "8");
 
@@ -35,7 +40,7 @@ function testPipeline(beforeExit){
     try{ p.order(); }
     catch(e){ ok(false); }
 
-    eq(p.order(), ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
+    eq(p.order(), ['0', '0.5', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
 
     beforeExit(function(){ eq(calls, expectedCalls); });
 }
