cfe665d28de87fcf44b19745b813b6f10b4d1b6b
Kendrick Taylor <kendrick@circuitsix.com>
2016-02-15 23:10:36 -0800

    add better phone number formatting

--- dry.underscore.js
+++ dry.underscore.js
@@ -3245,11 +3245,20 @@ function library(_){
 
     format.phoneNumber = format.phone_number = function(number, locale){
         if(!_.isString(number)){ return(number); }
+        var source = number;
+        number = _.numbers(number);
         if(number.length === 10){
             return(number.substr(0, 3) + "-" + number.substr(3, 3) + "-" + number.substr(6, 4));
         }else if(number.length === 11){
-            return(number.substr(0, 1) + "-" + number.substr(1, 3) + "-" + number.substr(4, 3) + "-" + number.substr(7, 4));
-        }else{ return(number); }
+            if(source[0] === "+"){
+                return("+" + number.substr(0, 1) + " " + number.substr(1, 3) + "-" + number.substr(4, 3) + "-" + number.substr(7, 4));
+            }else{
+                return(number.substr(0, 1) + "-" + number.substr(1, 3) + "-" + number.substr(4, 3) + "-" + number.substr(7, 4));
+            }
+        }else if(number.length === 12 && number[0] === "+"){
+            number = number.substr(1, number.length-1);
+            return("+" + number.substr(0, 1) + " " + number.substr(1, 3) + "-" + number.substr(4, 3) + "-" + number.substr(7, 4));
+        }else{ return(source); }
     }
 
     format.number = function(num, round, use_fixed){
@@ -4903,110 +4912,110 @@ function mixin(_){
 _.hook = (
 function (_){
 
-var hooker = function(f){
+    var hooker = function(f){
 
-    if(f === undefined){ f = {}; }
-        
-    f.hook_parent = _.rw("_hook_parent");
+        if(f === undefined){ f = {}; }
+            
+        f.hook_parent = _.rw("_hook_parent");
 
-    f.hook = function(event, callback, is_error_handler){
-        var self = this;
-        if(!event || !callback){ _.fatal("You must provide an event name and a function."); }
-        event = event.toLowerCase();
-        if(self._hooks === undefined){ self._hooks = {}; }
-        if(self._hooks[event] === undefined){ self._hooks[event] = []; }
+        f.hook = function(event, callback, is_error_handler){
+            var self = this;
+            if(!event || !callback){ _.fatal("You must provide an event name and a function."); }
+            event = event.toLowerCase();
+            if(self._hooks === undefined){ self._hooks = {}; }
+            if(self._hooks[event] === undefined){ self._hooks[event] = []; }
 
-        self._hooks[event].push({ handler: callback, is_error_handler: is_error_handler });
-    };
+            self._hooks[event].push({ handler: callback, is_error_handler: is_error_handler });
+        };
 
-    f.hooks = function(event){
-        var hooks = [];
-        var parent = this.hook_parent();
-        if(parent){ hooks = parent.hooks(event); }
+        f.hooks = function(event){
+            var hooks = [];
+            var parent = this.hook_parent();
+            if(parent){ hooks = parent.hooks(event); }
 
-        if(this._hooks && this._hooks[event]){
-            hooks = _.concat(hooks, this._hooks[event]);
-        }
-
-        return(hooks);
-    };
-
-    f.unhook = function(event, callback){
-        var self = this;
-        
-        if(!self._hooks){ return; }
-        
-        if(!event || !callback){ _.fatal("You must provide an event name and a function."); }
-        event = event.toLowerCase();
-
-        if(self._hooks && self._hooks[event]){
-            self._hooks[event] = _.filter(self._hooks[event], function(val){
-                if(_.isFunction(callback)){
-                    return(val.handler !== callback);                    
-                }
-            });
-        }
-    };
-
-    f.bite = function(event, args, callback, timeout){
-        var self = this;
-
-        if(_.isFunction(args) && !callback){
-            callback = args;
-            args = [];
-        }
-
-        if(!_.isString(event) || !_.isFunction(callback)){
-            _.fatal("You must provide an event name and a function.");
-        }
-        event = event.toLowerCase();
-        
-        var hooks = self.hooks(event);
-
-        var error = null;
-        _.each.async(hooks, function(val, key, next, end){
-            // var to = _.timeout("Hook for event: " + event + " doesn't return in a timely manner, it probably forgot to call next.", 5000);
-
-            if(error && !val.is_error_handler){ return next(); }
-
-            function next_handler(err){
-
-                if(error && !err){ error.swallowed = true; }
-                else if(error && error !== err){ 
-                    err.original = error;
-                    err.rewritten = true;
-                    error = err;
-                }else if(err){ error = err; }
-
-                next();
+            if(this._hooks && this._hooks[event]){
+                hooks = _.concat(hooks, this._hooks[event]);
             }
 
-            var handler = next_handler;
-            if(timeout){ handler = _.timeout(next_handler, timeout); }
+            return(hooks);
+        };
 
-            var handler_args = _.concat(handler, args);
-            if(val.is_error_handler){ handler_args.unshift(error); }
+        f.unhook = function(event, callback){
+            var self = this;
+            
+            if(!self._hooks){ return; }
+            
+            if(!event || !callback){ _.fatal("You must provide an event name and a function."); }
+            event = event.toLowerCase();
 
-            val.handler.apply(null, handler_args);
+            if(self._hooks && self._hooks[event]){
+                self._hooks[event] = _.filter(self._hooks[event], function(val){
+                    if(_.isFunction(callback)){
+                        return(val.handler !== callback);                    
+                    }
+                });
+            }
+        };
 
-        }, function(){
-            if(error){ return callback(error); }
-            else{ callback.apply(null, _.concat(null, args)); }
+        f.bite = function(event, args, callback, timeout){
+            var self = this;
+
+            if(_.isFunction(args) && !callback){
+                callback = args;
+                args = [];
+            }
+
+            if(!_.isString(event) || !_.isFunction(callback)){
+                _.fatal("You must provide an event name and a function.");
+            }
+            event = event.toLowerCase();
+            
+            var hooks = self.hooks(event);
+
+            var error = null;
+            _.each.async(hooks, function(val, key, next, end){
+                // var to = _.timeout("Hook for event: " + event + " doesn't return in a timely manner, it probably forgot to call next.", 5000);
+
+                if(error && !val.is_error_handler){ return next(); }
+
+                function next_handler(err){
+
+                    if(error && !err){ error.swallowed = true; }
+                    else if(error && error !== err){ 
+                        err.original = error;
+                        err.rewritten = true;
+                        error = err;
+                    }else if(err){ error = err; }
+
+                    next();
+                }
+
+                var handler = next_handler;
+                if(timeout){ handler = _.timeout(next_handler, timeout); }
+
+                var handler_args = _.concat(handler, args);
+                if(val.is_error_handler){ handler_args.unshift(error); }
+
+                val.handler.apply(null, handler_args);
+
+            }, function(){
+                if(error){ return callback(error); }
+                else{ callback.apply(null, _.concat(null, args)); }
+            });
+        };
+        
+        return(f);
+    };
+
+    hooker.fun = function(event){
+        return(function(){
+            var args = _.a(arguments);
+            args.unshift(event);
+            return(this.hook.apply(this, args));
         });
     };
-    
-    return(f);
-};
 
-hooker.event_function = function(event){
-    return(function(){
-        var args = _.a(arguments);
-        args.unshift(event);
-        return(this.hook.apply(this, args));
-    });
-};
-
-return(hooker);
+    return(hooker);
 
 }
 )(_);
