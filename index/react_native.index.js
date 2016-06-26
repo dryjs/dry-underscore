2fb2527e074f39d2f616a75f82e7e16109c4ae13
Kendrick Taylor <kendrick@circuitsix.com>
2016-06-25 23:27:14 -0700

    react native works

--- index/react_native.index.js
+++ index/react_native.index.js
@@ -2,7 +2,7 @@
 var _ = require('./common.index.js');
 
 require('../lib/client/client.js').mixin(_);
-_.http = require('../lib/client/http.js').library(_);
+_.http = require('../lib/react_native/http.js').library(_);
 
 // timer requires _.stderr to be defined for defaults
 _.timer = require('../lib/timer.js').library(_);
